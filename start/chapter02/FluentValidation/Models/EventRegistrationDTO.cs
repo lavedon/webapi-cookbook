29bae0652f9d8ec7c0cefb129658d80f96c0ad58
lavedon <lavedon@gmail.com>
2024-06-09 17:58:42 -0400

    removed some stuff

--- start/chapter02/FluentValidation/Models/EventRegistrationDTO.cs
+++ start/chapter02/FluentValidation/Models/EventRegistrationDTO.cs
@@ -2,7 +2,7 @@ using System.ComponentModel.DataAnnotations;
 
 namespace events.Models;
 
-public class EventRegistrationDTO : IValidatableObject
+public class EventRegistrationDTO
 {
     public int Id { get; set; }
 
@@ -28,23 +28,4 @@ public class EventRegistrationDTO : IValidatableObject
     [Required]
     [Range(1, 7, ErrorMessage = "Number of days attending must be between 1 and 7.")]
     public int DaysAttending { get; set; }
-
-    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
-    {
-        if (EventDate < DateTime.Now)
-        {
-            yield return new ValidationResult(
-                "Event date must be in the future.",
-                new[] { nameof(EventDate) });
-
-        }
-
-        if ((FullName.Contains("Garry") || FullName.Contains("Luke")) && EventName == "C# Conference")
-            {
-                yield return new ValidationResult(
-                    $"{FullName} is banned from {EventName}.",
-                    new[] { nameof(FullName), nameof(EventName) });
-            }
-
-    }
 }
