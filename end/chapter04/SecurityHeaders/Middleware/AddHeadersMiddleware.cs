174f048b1e6d328ced98d2e9d58f650a8aca3bcb
lavedon <lavedon@gmail.com>
2024-07-14 04:33:42 -0400

    clean upp

--- end/chapter04/SecurityHeaders/Middleware/AddHeadersMiddleware.cs
+++ end/chapter04/SecurityHeaders/Middleware/AddHeadersMiddleware.cs
@@ -11,9 +11,9 @@ public class AddHeadersMiddleware
     {
         context.Response.OnStarting(() =>
         {
-            context.Response.Headers.Add("X-Frame-Options", "DENY");
-            context.Response.Headers.Add("X-Content-Type-Options", "nosniff");
-            context.Response.Headers.Add("Content-Security-Policy", "default-src 'self'; script-src 'self'; object-src 'none'; frame-ancestors 'none'; base-uri 'self';");
+            context.Response.Headers.Append("X-Frame-Options", "DENY");
+            context.Response.Headers.Append("X-Content-Type-Options", "nosniff");
+            context.Response.Headers.Append("Content-Security-Policy", "default-src 'self'; script-src 'self'; object-src 'none'; frame-ancestors 'none'; base-uri 'self';");
 
             return Task.CompletedTask;
         });
