1680a84cd427476ab98a444beeb8023e2bb43064
lavedon <lavedon@gmail.com>
2024-08-16 05:10:55 -0400

    powershell one was not meant to be

--- end/chapter06/PowershellSignalR/opinionPoll/Models/Vote.cs
+++
@@ -1,7 +0,0 @@
-public class Vote
-{
-    public int Id { get; set; }
-    public int Choice { get; set; }
-    public DateTime Timestamp { get; set; }
-    public string ConnectionId { get; set; } = String.Empty;
-}
