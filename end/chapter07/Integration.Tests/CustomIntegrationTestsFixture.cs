b8edeac624c6e0a7436e2e26420730f945982be0
lavedon <lavedon@gmail.com>
2024-09-15 16:35:46 -0400

    I think we have a good starter project for with auth

---
+++ end/chapter07/Integration.Tests/CustomIntegrationTestsFixture.cs
@@ -0,0 +1,61 @@
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using books.Data;
+using books;
+using System.Net.Http;
+using Microsoft.AspNetCore.Hosting;
+
+namespace Tests.Integration;
+
+public class CustomIntegrationTestsFixture : WebApplicationFactory<Program>
+{
+    private SqliteConnection _connection;
+
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+            builder.ConfigureServices(services =>
+            {
+                var descriptor = services.SingleOrDefault(d => d.ServiceType == typeof(DbContextOptions<AppDbContext>));
+
+                if (descriptor != null) 
+                {
+                    services.Remove(descriptor);
+                }
+
+                _connection = new SqliteConnection("DataSource=:memory:");
+                _connection.Open();
+
+                services.AddDbContext<AppDbContext>(options =>
+                {
+                    options.UseSqlite(_connection);
+                });
+
+                var sp = services.BuildServiceProvider();
+
+                using (var scope = sp.CreateScope())
+                {
+                    var scopedServices = scope.ServiceProvider;
+                    var db = scopedServices.GetRequiredService<AppDbContext>();
+
+                    db.Database.EnsureCreated();
+                    Utilities.InitializeDatabase(db);
+                }
+            });
+        }
+        
+        public HttpClient GetClient()
+        {
+            return CreateClient();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+            if (disposing)
+            {
+                _connection?.Close();
+            }
+        }
+}
